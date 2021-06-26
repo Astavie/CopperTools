9b959235214cb0b7423dc49f2c9b2dc4f1e2bda2
Astavie <astavie@protonmail.com>
2021-06-26 21:15:41 +0200

    recipes

--- src/main/java/astavie/coppertools/CopperTools.java
+++ src/main/java/astavie/coppertools/CopperTools.java
@@ -15,7 +15,7 @@ import net.minecraft.util.registry.Registry;
 public class CopperTools implements ModInitializer {
 
 	public static final ToolItem COPPER_SHOVEL = new ShovelItem(CopperToolMaterial.INSTANCE, 1.5F, -3.0F, new Item.Settings().group(ItemGroup.TOOLS));
-    public static final ToolItem COPPER_SWORD = new SwordItem(CopperToolMaterial.INSTANCE, 3, -2.4F, new Item.Settings().group(ItemGroup.COMBAT));
+	public static final ToolItem COPPER_SWORD = new SwordItem(CopperToolMaterial.INSTANCE, 3, -2.4F, new Item.Settings().group(ItemGroup.COMBAT));
 	public static final ToolItem COPPER_PICKAXE = new PickaxeItem(CopperToolMaterial.INSTANCE, 1, -2.8F, new Item.Settings().group(ItemGroup.TOOLS)) {};
 	public static final ToolItem COPPER_AXE = new AxeItem(CopperToolMaterial.INSTANCE, 7.0F, -3.2F, new Item.Settings().group(ItemGroup.TOOLS)) {};
 	public static final ToolItem COPPER_HOE = new HoeItem(CopperToolMaterial.INSTANCE, -1, -2.0F, new Item.Settings().group(ItemGroup.TOOLS)) {};
